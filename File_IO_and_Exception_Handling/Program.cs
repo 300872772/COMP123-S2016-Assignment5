e02e225c2403ee02692ef8187774e73c6f83d4ab
300872772 <mamun_131@hotmail.com>
2016-07-20 00:33:10 -0300

    added added grade file

--- File_IO_and_Exception_Handling/Program.cs
+++ File_IO_and_Exception_Handling/Program.cs
@@ -3,6 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*  
+*Author: Md Mamunur Rahman  
+* Student ID: 300872772   
+*  
+* Date last modified: July 20, 2016  
+* Description: This program read text file and handle exceptions   
+*   
+* Version: 0.0.1 - added added grade file 
+*/ 
 
 namespace File_IO_and_Exception_Handling
 {
